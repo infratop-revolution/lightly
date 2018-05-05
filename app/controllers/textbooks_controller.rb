374af39a42fe42fc5b8c43a7c8b77c5fc88dc104
平山大稀 <hirayamadaiki@hirayamadaimarenoMacBook-Pro.local>
2018-05-05 17:25:47 +0900

    final

--- app/controllers/textbooks_controller.rb
+++ app/controllers/textbooks_controller.rb
@@ -2,6 +2,7 @@ class TextbooksController < ApplicationController
   def index
   	@q = Text.ransack(params[:q])
   	@textbooks = @q.result(distinct: true)
+    @request = Request.new
   end
 
   def show
@@ -10,6 +11,7 @@ class TextbooksController < ApplicationController
   def search
   	@q = Text.search(search_params)
   	@textbooks = @q.result(distinct: true)
+    @request = Request.new
   end
   private
    def search_params
