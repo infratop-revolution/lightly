b1b9983c8d1b63eb2bb1534b6fda3907daa69391
yamamoto7 <yamamoto.kenta.infratop@gmail.com>
2018-05-05 10:39:27 +0900

    add request

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,4 +1,16 @@
 class RequestsController < ApplicationController
   def index
+  	@request = Request.new
+  	@requests = Request.all
+  end
+  def create
+  	request = Request.new
+  	request.applicant_id = 1
+  	request.approver_id = 2
+  	applicant_frag = true
+  	approver_frag = true
+  	status = 0
+  	request.save
+  	redirect_to chats_index_path
   end
 end
