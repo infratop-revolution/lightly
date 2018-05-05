fb02c7708b15a8af68759fc01eaf0aeac494bf2d
yuu <yamaguchi.yudai.infratop@gmail.com>
2018-05-05 10:52:35 +0900

    commit

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,17 @@
 class ListsController < ApplicationController
   def new
-  	@listsnew = TextList.new
-  end
-
-  def edit
-  	
+  	@list = TextList.new
   end
 
   def create
+  	@list = TextList.new
+    @list.user_id = current_user.id
+  	@list.save
+    redirect_to textbooks_path
+  end
+
+  def edit
+  	@list = TextList.find(params[:id])
   end
 
   def update
