25d98c0563f19817e574818a605c94271dc1a38f
yamamoto7 <yamamoto.kenta.infratop@gmail.com>
2018-05-05 15:47:28 +0900

    reset

--- db/migrate/20180504094747_create_texts.rb
+++ db/migrate/20180504094747_create_texts.rb
@@ -3,6 +3,8 @@ class CreateTexts < ActiveRecord::Migration[5.1]
     create_table :texts do |t|
       t.string :name
       t.integer :test_list_id
+      t.integer :text_list_id
+      t.integer :user_id
 
       t.timestamps
     end
