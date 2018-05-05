501fb01c10daf96da53c68f607a1aa9b0859cce0
yamamoto7 <yamamoto.kenta.infratop@gmail.com>
2018-05-05 14:40:38 +0900

    add user karamu

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,17 +2,4 @@ class UsersController < ApplicationController
   def show
   end
 
-
-  def hotel_params
-<<<<<<< HEAD
-        params.require(:list).permit(
-          :user_id, :test_list_id,
-          lists_attributes: [:id, :user_id, :test_list_id, :capacity, :note, :_destroy]
-=======
-        params.require(:hotel).permit(
-          :user_id, :test_list_id,
-          roomtypes_attributes: [:id, :hotel_id, :name, :capacity, :note, :_destroy]
->>>>>>> 3c2c5d2e444339d780bb056d4b23ea3bf160abbc
-        )
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+	devise_for :users
+
   get 'textbooks/search' => 'textbooks#search'
   get 'chats/index'
 
